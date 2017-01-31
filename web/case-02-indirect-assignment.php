7595dc06dfb867c7f0be1116b4b079dc57a95d76
Ben Peachey <potherca@gmail.com>
2017-01-31 20:04:24 +0000

    ADD: Adds minimal test-case for indirect closure assignment.

---
+++ web/case-02-indirect-assignment.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace PhpTal\Issue\Issue57;
+
+use \PHPTAL;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
+$template = <<<'HTML'
+<html>
+    <meta charset="utf-8"/>
+    <title>Example Code to demonstrate issue 57</title>
+    <p>
+        <span tal:content="foo"></span>
+    </p>
+</html>
+HTML;
+
+$engine = new PHPTAL();
+
+$engine->setSource($template);
+
+// Direct asignment
+$foo = function () {
+    return 'Foo';
+};
+
+$engine->foo = $foo;
+
+try {
+    $result = $engine->execute();
+} catch (Exception $exception){
+    $result = (string) $exception;
+}
+
+echo $result;
+
+/*EOF*/
